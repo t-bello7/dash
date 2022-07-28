d4f1c5e63176aa9b5d6988f06e8e26fd305becaf
t-bello7 <bello4aus@gmail.com>
2022-07-29 00:23:58 +0100

    add encrypt, cloudinary util
    create template for publishing to mutiple blog

--- pages/api/publish/[id].js
+++ pages/api/publish/[id].js
@@ -1,7 +1,8 @@
 import { getSession } from 'next-auth/react';
 import prisma from "../../../lib/prisma";
+import axios from 'axios';
 
-const DEV_TO_BASE_URL = 'https://dev.to/api/articles'
+const DEVTO_BASE_URL = 'https://dev.to/api/articles'
 const MEDIUM_BASE_URL = 'https://api.medium.com/v1'
 const HASHNODE_BASE_URL = 'https://api.hashnode.com/'
 
@@ -11,13 +12,32 @@ export default async function handle(req, res){
 
     const postId = req.query.id
 
-    // addd the sprinkle of pushing the article to various blog platforms
     const post = await prisma.post.update({
         where: {id: Number(postId)},
         data: { publised: true}
     })
     res.json(post)
+    // addd the sprinkle of pushing the article to various blog platforms
+    // -- >get post data with id 
 
+    // -- >check connected blogs 
+    // -- >for each of the blog write thier various post request 
+    // medium 
+
+    function postToMedium () {
+      return axios.post(MEDIUM_BASE_URL, post )
+    }
+    function postToHashnode () {
+      return axios.post(HASHNODE_BASE_URL, post )
+    }
+    function postToDev () {
+      return axios.post(DEVTO_BASE_URL, post )
+    }
+    Promise.all([postToMedium(), postToHashnode(), postToDev()])
+    .then((res) => {
+      console.log(res[0], res[1], res[2])
+    })
+    .catch((e) => console.log(e))
     
  // } else {
     // res.send({
